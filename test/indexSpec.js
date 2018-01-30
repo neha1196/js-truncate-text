533e4c94bff53dca75c6eec319450bfc0585e436
neha1196 <neha@nineleaps.com>
2018-01-30 22:59:50 +0530

    Critical bug in version 1.0.1 fixed

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -1,22 +1,24 @@
+"use strict";
+
 var app = require("../index.js");
 
-describe("Truncate", function() {
-  var inputString = "This is truncate text";
+describe("Truncate", function () {
+    var inputString = "This is truncate text";
 
-  it("should split strings by spaces", function() {
-    var splitInput = inputString.split(" ");
-    expect(splitInput)
-      .toEqual(['This', 'is', 'truncate', 'text']);
-  });
+    it("should split strings by spaces", function () {
+        var splitInput = inputString.split(" ");
+        expect(splitInput)
+            .toEqual(['This', 'is', 'truncate', 'text']);
+    });
 
-  it("should find the length of string", function() {
-    expect(inputString.length)
-      .toEqual(21);
-  });
+    it("should find the length of string", function () {
+        expect(inputString.length)
+            .toEqual(21);
+    });
 
-  it("should set the defaut truncation limit if not specified", function() {
-    expect(inputString.length)
-      .toEqual(21);
-  });
+    it("should set the defaut truncation limit if not specified", function () {
+        expect(inputString.length)
+            .toEqual(21);
+    });
 
 });
